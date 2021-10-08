652703c473b9d0d0d9d3ced5f1c5dc436adbe897
sergey leonov <redfortten@hotmail.com>
2021-10-08 23:45:52 +0300

    Добавьте файлы проекта.

---
+++ lab7.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Два треугольника заданы длинами сторон. Определить, площадь какого из них больше. Использовать метод.
+            #region Инициализация переменных
+            double a1, b1, c1, a2, b2, c2;
+            Console.WriteLine("Введите стороны первого треугольника");
+            a1 = Convert.ToDouble(Console.ReadLine());
+            b1 = Convert.ToDouble(Console.ReadLine());
+            c1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите стороны второго треугольника");
+            a2 = Convert.ToDouble(Console.ReadLine());
+            b2 = Convert.ToDouble(Console.ReadLine());
+            c2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            #endregion
+            if ((CalcArea(a1, b1, c1)) > (CalcArea(a2, b2, c2)))
+            {
+                Console.WriteLine("Площадь первого треугольника больше");
+            }
+            else
+            {
+                Console.WriteLine("Площадь второго треугольника больше");
+            }
+            Console.WriteLine("Для завершения нажмите любую клавишу");
+            Console.ReadKey();
+        }
+        static double CalcArea(double a, double b, double c)
+        {
+            double p, S;
+            p = (a + b + c) / 2; // Полупериметр
+            S = Math.Round(Math.Sqrt(p * (p - a) * (p - b) * (p - c)), 2); // Площадь треугольника
+            return S;
+        }
+    }
+}
---
+++ lab7.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создать метод для вычисления объема и площади поверхности куба по длине его ребра.
+            double c, area, volume;
+            Console.WriteLine("Введите длину ребра куба");
+            c = Convert.ToDouble(Console.ReadLine());            
+            CalcCube(c, out area, out volume);
+            Console.WriteLine("Площадь поверхности куба равна = {0}", area);
+            Console.WriteLine("Объем куба равен = {0}", volume);
+            Console.WriteLine("Для завершения нажмите любую клавишу");
+            Console.ReadKey();
+        }
+        static void CalcCube(double c, out double area, out double volume)
+        {            
+            area = 6 * Math.Pow((c), 2);
+            volume = Math.Pow((c), 3);                  
+        }
+    }
+}
